e713e3e568a3f0607ea3ab1b42b3cc877f7513d2
hazemmohamed11 <37817913+hazemmohamed11@users.noreply.github.com>
2023-12-07 17:18:31 +0200

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,72 @@
+<?php
+// profile.php
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+// Assuming you have a database connection
+$host = 'localhost';
+$db = 'diss&miss';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+} catch (PDOException $e) {
+    die("Error: " . $e->getMessage());
+}
+
+// Sample data from frontend (you need to replace this with actual data from your frontend)
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (!isset($data['user_id'], $data['token'])) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Invalid data']);
+    exit;
+}
+
+$secret_key = "hazem";
+$token = $data['token'];
+
+function custom_jwt_decode($jwt, $key) {
+    $parts = explode('.', $jwt);
+    if (count($parts) !== 3) {
+        return false;
+    }
+
+    list($header, $payload, $signature) = $parts;
+
+    $verified_signature = hash_hmac('sha256', $header . '.' . $payload, $key, true);
+    $verified_signature_base64 = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($verified_signature));
+
+    if ($signature !== $verified_signature_base64) {
+        return false;
+    }
+
+    return json_decode(base64_decode($payload), true);
+}
+
+$decoded = custom_jwt_decode($token, $secret_key);
+
+if (!$decoded || $decoded['user_id'] !== $data['user_id']) {
+    http_response_code(401);
+    echo json_encode(['error' => 'Invalid token or user_id']);
+    exit;
+}
+
+$user_id = $data['user_id'];
+
+// Fetch user profile information
+$stmt = $pdo->prepare("SELECT * FROM userprofile WHERE user_id = :user_id");
+$stmt->bindParam(':user_id', $user_id);
+
+if ($stmt->execute()) {
+    $profileData = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo json_encode($profileData);
+} else {
+    http_response_code(500);
+    echo json_encode(['error' => 'Failed to fetch user profile']);
+}
+?>
