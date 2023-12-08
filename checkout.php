b42ca741d5fc219469b2b4d92d2ffcf4f13cecd2
hazemmohamed11 <37817913+hazemmohamed11@users.noreply.github.com>
2023-12-08 18:31:02 +0200

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -102,15 +102,29 @@ try {
 
     // Check and apply voucher discount
     if (!empty($data['voucher_code'])) {
-        $voucherSql = "SELECT discount FROM vouchers WHERE voucher_code = :voucher_code";
+        $voucherSql = "SELECT voucher_id, discount FROM vouchers WHERE voucher_code = :voucher_code AND status = 'active'";
         $voucherStmt = $pdo->prepare($voucherSql);
         $voucherStmt->bindParam(':voucher_code', $data['voucher_code']);
         $voucherStmt->execute();
-        $voucherDiscount = $voucherStmt->fetchColumn();
+        $voucherData = $voucherStmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($voucherDiscount) {
+        if ($voucherData) {
+            $voucherId = $voucherData['voucher_id'];
+            $voucherDiscount = $voucherData['discount'];
+
             // Apply voucher discount to total cost
             $totalCost -= $voucherDiscount;
+
+            // Update the voucher status to 'used'
+            $updateVoucherSql = "UPDATE vouchers SET status = 'used' WHERE voucher_id = :voucher_id";
+            $updateVoucherStmt = $pdo->prepare($updateVoucherSql);
+            $updateVoucherStmt->bindParam(':voucher_id', $voucherId);
+            $updateVoucherStmt->execute();
+        } else {
+            // Handle case where the voucher is not found or not active
+            http_response_code(400);
+            echo json_encode(['error' => 'Invalid voucher code or voucher is not active']);
+            exit;
         }
     }
 
